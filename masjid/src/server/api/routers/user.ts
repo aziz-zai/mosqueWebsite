66453fe6548847b3952899bae6f29ca6012bfe6f
aziz <80921378+aziz-zai@users.noreply.github.com>
2024-03-10 14:44:53 +0100

    Update user.ts

--- masjid/src/server/api/routers/user.ts
+++ masjid/src/server/api/routers/user.ts
@@ -11,19 +11,6 @@ export const userRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.user.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.user.findFirst({
       orderBy: { createdAt: "desc" },
